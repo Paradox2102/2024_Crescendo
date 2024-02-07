d3d044987692869c44f99b52e9c9336d54760f1c
Briselda <122310852+BrisMR@users.noreply.github.com>
2024-02-06 17:59:53 -0800

    elevator subsystem

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -5,11 +5,10 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkFlex;
-import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
-import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
@@ -18,6 +17,14 @@ public class ElevatorSubsystem extends SubsystemBase {
   private CANSparkFlex m_elevatorMotor = new CANSparkFlex(Constants.ElevatorConstants.k_elevatorMotor, MotorType.kBrushless);
   private RelativeEncoder m_elevatorEncoder;
   private double m_elevatorPoint;
+
+  private static final double k_p = 0;
+  private static final double k_i = 0;
+  private static final double k_d = 0;
+  private static final double k_iZone = 0;
+  private static final double k_deadzone = 0;
+  private PIDController m_PID = new PIDController(k_p, k_i, k_d);
+
 
   private double m_power;
 
@@ -44,9 +51,13 @@ public class ElevatorSubsystem extends SubsystemBase {
 
   @Override
   public void periodic() {
+    //setPosition perodics
+    
+    //ToggleElevatorPosition(start high-low)normal command start 20 inches (constants- max instant -20, min extent -0); Set pibit
+    //manual command - phonix elevator
     //power
     SmartDashboard.putNumber("Elevator Power", m_power);
-    //show how the elevator is positioned
+    //show displays on the SmartDasboard --> where elevator is positioned
     SmartDashboard.putNumber("Elevator Raw Position", getRawElevatorPosition());
     SmartDashboard.putNumber("Elevator Cooked Position", getCookedElevatorPosition());
     // This method will be called once per scheduler run

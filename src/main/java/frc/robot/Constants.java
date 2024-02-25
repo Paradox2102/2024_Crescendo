cf8160db9aa4a8bc9990ffb0b2af3e01aa7c0fe0
paul <pauljregnier@gmail.com>
2024-02-25 10:35:13 -0800

    moved intake pos down

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -194,7 +194,7 @@ public final class Constants {
     public static final double k_pivotTicksToDegrees = 360;
 
     // Positions
-    public static double k_intakePositionDegrees = 110;
+    public static double k_intakePositionDegrees = 112;
     public static double k_resetPositionDegrees = 4;
     public static double k_ampPositionDegrees = -5;
 

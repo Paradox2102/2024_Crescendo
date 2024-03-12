0a6488d8c8be4562fd3aca87b87fd4d276dced72
Briselda <122310852+BrisMR@users.noreply.github.com>
2024-03-11 18:51:41 -0700

    update

--- src/main/java/frc/robot/subsystems/StickSubsystem.java
+++ src/main/java/frc/robot/subsystems/StickSubsystem.java
@@ -2,7 +2,7 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 /* 
-55 neosparkmaxes
+55 cansparkmaxes
 relative emcoder 
 setPoint
 rachandpinion - motors run by a it (so changing the motors changes both)
@@ -12,12 +12,15 @@ rachandpinion - motors run by a it (so changing the motors changes both)
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class StickSubsystem extends SubsystemBase {
   // private CANSparkMax m_leftMotor = ;
   // private CANSparkMax m_rightMotor = ;
+
+  private RelativeEncoder m_stickEncoder;
   /** Creates a new StickSubsystem. */
   public StickSubsystem() {}
 

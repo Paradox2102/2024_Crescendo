3cfc058510f3697c713dcfb4754793057d9e2b96
Briselda <122310852+BrisMR@users.noreply.github.com>
2024-03-11 18:45:40 -0700

    Create StickSubsystem.java
    Created StickSubsystem

---
+++ src/main/java/frc/robot/subsystems/StickSubsystem.java
@@ -0,0 +1,28 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+/* 
+55 neosparkmaxes
+relative emcoder 
+setPoint
+rachandpinion - motors run by a it (so changing the motors changes both)
+
+13 & 14
+*/
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class StickSubsystem extends SubsystemBase {
+  // private CANSparkMax m_leftMotor = ;
+  // private CANSparkMax m_rightMotor = ;
+  /** Creates a new StickSubsystem. */
+  public StickSubsystem() {}
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}

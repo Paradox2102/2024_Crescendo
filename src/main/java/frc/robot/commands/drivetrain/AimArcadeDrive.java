c93f092bef90c8a6a3f6c70680cdf1377b2a47eb
paul <pauljregnier@gmail.com>
2024-03-18 18:54:33 -0700

    Changes

---
+++ src/main/java/frc/robot/commands/drivetrain/AimArcadeDrive.java
@@ -0,0 +1,75 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.drivetrain;
+
+import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.apriltagsCamera.Logger;
+import frc.robot.Constants;
+import frc.robot.subsystems.DriveSubsystem;
+
+public class AimArcadeDrive extends Command {
+     /** Creates a new ArcadeDrive. */
+
+  private DriveSubsystem m_subsystem;
+  private DoubleSupplier m_getX;
+  private DoubleSupplier m_getY;
+  private Trigger m_slowMode;
+  private Trigger m_slowMode1;
+
+  public AimArcadeDrive(DriveSubsystem driveSubsystem, DoubleSupplier getX, DoubleSupplier getY, Trigger slowMode, Trigger slowMode1) {
+    m_subsystem = driveSubsystem;
+    m_getX = getX;
+    m_getY = getY;
+    m_slowMode = slowMode;
+    m_slowMode1 = slowMode1;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(m_subsystem);
+  }
+  //
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    Logger.log("ArcadeDrive", 0, "initialize");
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    double x = -MathUtil.applyDeadband(m_getX.getAsDouble(), Constants.DriveConstants.k_driveDeadband);
+    double y = -MathUtil.applyDeadband(m_getY.getAsDouble(), Constants.DriveConstants.k_driveDeadband);
+    double rot = MathUtil.applyDeadband(m_subsystem.orientPID(m_subsystem.getFutureRotationalGoalFromTargetDegrees()), 0);
+    if (m_slowMode.getAsBoolean() || m_slowMode1.getAsBoolean()) {
+      x *= .3;
+      y *= .3;
+    }
+    m_subsystem.drive(
+      y, 
+      x, 
+      rot, 
+      true, 
+      true,
+      Constants.DriveConstants.k_rotatePoint
+    );
+
+    
+    // m_swerve.setModuleStates(m_defaultState);
+    // System.out.println(String.format("x=%f, y=%f, rot=%f, isFieldRelative=%b", x, y, rot, isFieldRelative)); 
+  }
+  
+  
+
+  // Called once the command ends or is interrupted.
+  public void end() {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}

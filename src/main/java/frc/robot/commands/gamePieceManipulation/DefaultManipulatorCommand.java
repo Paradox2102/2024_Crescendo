0d4038e8558b6b17d4a1634d6dee13e45d0ea5f2
bovlb <jgaby@gabysoft.com>
2024-07-02 17:09:40 -0700

    Started redoing the complicated code so that it is easier to understand

--- src/main/java/frc/robot/commands/gamePieceManipulation/DefaultManipulatorCommand.java
+++ src/main/java/frc/robot/commands/gamePieceManipulation/DefaultManipulatorCommand.java
@@ -2,28 +2,41 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-
 package frc.robot.commands.gamePieceManipulation;
 
+import java.util.concurrent.CyclicBarrier;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.revrobotics.AbsoluteEncoder;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.ManipulatorSubsystem;
 import frc.robot.Constants;
+
 @SuppressWarnings("unused")
 public class DefaultManipulatorCommand extends Command {
   /** Creates a new DefaultStowGamePiece. */
-  //sucks the game piece into the storage position. If the front sensor is broken then the game piece is not in the correct position, but if the back one is broken and the front is not then the piece is correctly stowed.
+  // sucks the game piece into the storage position. If the front sensor is broken
+  // then the game piece is not in the correct position, but if the back one is
+  // broken and the front is not then the piece is correctly stowed.
   ManipulatorSubsystem m_subsystem;
   DriveSubsystem m_driveSubsytem;
   boolean m_isFront;
   private final double k_revRangeMeters = 10;
+  private State m_state;
+
+  private enum State {
+    empty,
+    intaking,
+    holding
+  };
 
   public DefaultManipulatorCommand(ManipulatorSubsystem subsystem, DriveSubsystem driveSubsystem, boolean isFront) {
     m_subsystem = subsystem;
     m_driveSubsytem = driveSubsystem;
     m_isFront = isFront;
+
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(m_subsystem);
   }
@@ -31,40 +44,40 @@ public class DefaultManipulatorCommand extends Command {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-
+    m_state = State.empty;
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    if (Constants.States.m_hasGamePiece&&!Constants.States.m_isGamePieceStowed){
-double adjustPower = m_isFront? Constants.ShooterConstants.k_adjustGamePiecePower:Constants.HolderConstants.k_adjustGamePiecePower;
-m_subsystem.setPower(Constants.States.m_shootIntakeSide?-adjustPower:adjustPower);
-    }else if(Constants.States.m_isGamePieceStowed&&m_driveSubsytem.getTranslationalDistanceFromSpeakerMeters()<k_revRangeMeters && Constants.States.m_autoRotateAim){
-      //Front
-      if(m_isFront){
-        if(Constants.States.m_shootIntakeSide){
-          m_subsystem.setVelocityRPM(Constants.States.m_speakerMode? m_subsystem.getRevSpeed():Constants.ShooterConstants.k_ampShootVelocityRPM);
-        }else{
-          m_subsystem.stop();
-        }
-      }else{//Back
-      if(Constants.States.m_shootIntakeSide){
-        m_subsystem.stop();
-      }else{
-        m_subsystem.setVelocityRPM(-m_subsystem.getRevSpeed());
-      }
-      }
-      
-      
-    }else{
-      m_subsystem.stop();
-    }
-  }
+    switch (m_state) {
+      case empty:
+handleEmptyState();
+        break;
 
+      case intaking:
+handleIntakingState();
+        break;
+
+      case holding:
+handleHoldingState();
+        break;
+    }
+
+  }
+void handleEmptyState(){
+
+}
+void handleIntakingState(){
+
+}
+void handleHoldingState(){
+  
+}
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+  }
 
   // Returns true when the command should end.
   @Override

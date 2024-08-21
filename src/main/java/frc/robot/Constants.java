c09b2affb4703f685298cbeece6e54574cb12fb2
Briselda <122310852+BrisMR@users.noreply.github.com>
2024-08-20 17:20:50 -0700

    c

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -235,10 +235,10 @@ public final class Constants {
 
     public static final double k_driveRadius = .475953574;
 
-    public static double k_FLOffset = 3.14 - (Math.PI / 2); // 4.8
-    public static double k_FROffset = 3.14; // 5.33
-    public static double k_BLOffset = 3.14 + (Math.PI); // 0.16
-    public static double k_BROffset = 3.14 + (Math.PI / 2); // 4.87
+    public static double k_FLOffset = 2.42 - (Math.PI / 2); // 4.8
+    public static double k_FROffset = 1.61; // 5.33
+    public static double k_BLOffset = 1.38 + (Math.PI); // 0.16
+    public static double k_BROffset = 4.27 + (Math.PI / 2); // 4.87
 
     public static final int k_drivingMotorPinionTeeth = 14;
 

0d392851d36cfd95e821a202a1ec425054a3b262
cpaggron <66983454+cpaggron@users.noreply.github.com>
2024-10-12 10:56:43 -0700

    Update DriveSubsystem.java

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -409,7 +409,7 @@ public class DriveSubsystem extends SubsystemBase {
    * @param pose The pose to which to set the odometry.
    */
   public void resetOdometry(Pose2d pose) {
-    m_tracker.setXYAngle(pose.getX(), pose.getY(), m_gyro.getAngle());
+    m_tracker.setXYAngle(pose.getX(), pose.getY(), pose.getRotation().getDegrees());
   }
 
   /**

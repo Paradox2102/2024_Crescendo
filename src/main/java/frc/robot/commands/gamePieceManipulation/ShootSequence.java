dbda375092984255178b8bcfed719b502ac079fa
cpaggron <66983454+cpaggron@users.noreply.github.com>
2024-10-12 11:01:35 -0700

    Update ShootSequence.java

--- src/main/java/frc/robot/commands/gamePieceManipulation/ShootSequence.java
+++ src/main/java/frc/robot/commands/gamePieceManipulation/ShootSequence.java
@@ -28,7 +28,7 @@ public class ShootSequence extends SequentialCommandGroup {
     // addCommands(new FooCommand(), new BarCommand());
     addCommands(
       new ConditionalCommand(
-        // new DriveToPosition(driveSubsystem, new Pose2d(10, 10, new Rotation2d())), 
+        // new DriveToPosition(driveSubsystem, new Pose2d(4, 6, Rotation2d.fromDegrees(90))), 
         new InstantCommand(),
         new InstantCommand(() -> {Constants.States.m_slowMode = true;}), 
         () -> !Constants.States.m_speakerMode

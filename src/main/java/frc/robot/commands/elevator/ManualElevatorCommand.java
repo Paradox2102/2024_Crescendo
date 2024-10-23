dab79ba9372936849e87a06edc5120f9d46ca82c
paul <pauljregnier@gmail.com>
2024-10-22 17:14:21 -0700

    started a new elevator command

--- src/main/java/frc/robot/commands/elevator/ManualElevatorCommand.java
+++ src/main/java/frc/robot/commands/elevator/ManualElevatorCommand.java
@@ -47,7 +47,7 @@ public class ManualElevatorCommand extends Command {
     // } else if (position <= Constants.ElevatorConstants.k_minDistance && m_getY.getAsDouble() > 0) {
     //   m_subsystem.setPower(0);
     } else if (y == 0) {
-      m_subsystem.setPower(0);;
+      m_subsystem.setPower(0);;//TODO - are there two semicolons for a reason?
     } else {
       m_subsystem.setPower(y > 0 ? 1 : -1);
     }

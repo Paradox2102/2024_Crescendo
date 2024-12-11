6a0caaf9793da7fa1713c378ee656e4ad95dcacd
Briselda <122310852+BrisMR@users.noreply.github.com>
2024-12-10 16:17:52 -0800

    Update Constants.java

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -56,9 +56,9 @@ public final class Constants {
 
       // Drive
       DriveConstants.k_FLOffset = 4.9 - (Math.PI / 2);
-      DriveConstants.k_FROffset = 3.316;
-      DriveConstants.k_BLOffset = 5.247 + (Math.PI);
-      DriveConstants.k_BROffset = 6.11 + (Math.PI / 2);
+      DriveConstants.k_FROffset = 3.32;
+      DriveConstants.k_BLOffset = 2.16 + (Math.PI);
+      DriveConstants.k_BROffset = 6.16 + (Math.PI / 2);
       DriveConstants.k_maxSpeedMetersPerSecond = 5.74;
 
       // Front
